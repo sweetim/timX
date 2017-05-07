bd817056978c66ae2aa9700028e28a46f6700f00
Tim <hosweetim@gmail.com>
2017-05-07 21:15:22 +0900

    fixed links order

--- controllers/router.py
+++ controllers/router.py
@@ -1,14 +1,15 @@
 import os
+from collections import OrderedDict
 
 from google.appengine.ext.webapp import template
 
 def getView(current, data):
     path = os.path.join(os.path.dirname(__file__), '../views' ,'router.html')
 
-    router = {
-        "about": "/",
-        "projects": "/projects"
-    }
+    router = OrderedDict([
+        ("about", "/"),
+        ("projects", "/projects")
+    ])
 
     routerLinks = { k: v for k, v in router.iteritems() if k != current}
 
