e89c84f37aef245a76dd59f5ba2e4c5d897851b3
sweetim <hosweetim@gmail.com>
2018-07-01 23:42:17 +0900

    added bitbucket dist files

---
+++ dist/precache-manifest.321be6a19ad4e7854fb46b1ce33d2ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2da3b8cda6c2a1408fbd",
+    "url": "/bitbucket/js/chunk-vendors.2da3b8cd.js"
+  },
+  {
+    "revision": "e08944fbb1ac0611c6a8",
+    "url": "/bitbucket/js/app.e08944fb.js"
+  },
+  {
+    "revision": "810d212d9cea2cd4869e24685121b217",
+    "url": "/bitbucket/index.html"
+  },
+  {
+    "revision": "87a031926d91b39a7cf533bd41d5a68f",
+    "url": "/bitbucket/img/bitbucket_logo.87a03192.png"
+  },
+  {
+    "revision": "2da3b8cda6c2a1408fbd",
+    "url": "/bitbucket/css/chunk-vendors.30e3f49e.css"
+  },
+  {
+    "revision": "e08944fbb1ac0611c6a8",
+    "url": "/bitbucket/css/app.ada83a3e.css"
+  }
+];
